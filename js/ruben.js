804c380562725041dce4fd738e43332c305fa1de
ruben <rgj565@educa.madrid.org>
2025-02-20 18:09:27 +0100

    subida

---
+++ js/ruben.js
@@ -0,0 +1,8 @@
+let boton = document.getElementById("boton");
+let fondo = document.getElementById("fondo");
+
+boton.onclick = () => {
+  fondo.innerText = "ruben"; 
+
+  fondo.style.backgroundColor = "blue"; 
+};
